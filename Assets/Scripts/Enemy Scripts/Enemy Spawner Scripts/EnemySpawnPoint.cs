6b872d88b6ca47736e90c7b63095a990c9860c00
Anthony <anth19800@hotmail.com>
2020-11-29 22:18:22 -0500

    [Add] Assignment

--- Assets/Scripts/Enemy Scripts/Enemy Spawner Scripts/EnemySpawnPoint.cs
+++ Assets/Scripts/Enemy Scripts/Enemy Spawner Scripts/EnemySpawnPoint.cs
@@ -12,14 +12,37 @@ public class EnemySpawnPoint : MonoBehaviour
     public int maxSpawn = 5;
     public float spawnTimeInterval = 1.0f;
     float _pvtSpawnTimeInterval = 1.0f;
+    public bool usePool = true;
+    float radius = 0.0f;
     void Start()
     {
-        CreatePool();
-        SpawnEnemy();
+        if (usePool)
+            CreatePool();
+        radius = gameObject.GetComponent<SphereCollider>().radius;
+
     }
 
     void Update()
     {
+        _pvtSpawnTimeInterval -= Time.deltaTime;
+        if (_pvtSpawnTimeInterval <= 0.0f)
+        {
+            if (usePool)
+                SpawnEnemy();
+            else
+                SpawnEnemyUnoptimized();
+        }
+
+        if (!usePool)
+            for (int i = 0; i < spawnEnemies.Count; i++)
+            {
+                if (!spawnEnemies[i].activeSelf)
+                {
+                    Destroy(spawnEnemies[i]);
+                    spawnEnemies.RemoveAt(i);
+                }
+            }
+
     }
     void CreatePool()
     {
@@ -43,12 +66,23 @@ public class EnemySpawnPoint : MonoBehaviour
         if (spawnIndex == -1)
             return;//no availible enemy spawns
 
-        float radius = gameObject.GetComponent<SphereCollider>().radius;
 
         spawnEnemies[spawnIndex].transform.position = gameObject.transform.position + new Vector3(Random.Range(-radius, radius), 0.0f, Random.Range(-radius, radius)) * spawnRadiusScalar;
         spawnEnemies[spawnIndex].SetActive(true);
         _pvtSpawnTimeInterval = spawnTimeInterval;
     }
 
+    void SpawnEnemyUnoptimized()
+    {
+        if (spawnEnemies.Count == maxSpawn)
+            return;
+
+        var temp = GameObject.Instantiate(enemyPrefab);
+        temp.SetActive(true);
+        temp.transform.position = gameObject.transform.position + new Vector3(Random.Range(-radius, radius), 0.0f, Random.Range(-radius, radius)) * spawnRadiusScalar;
+        _pvtSpawnTimeInterval = spawnTimeInterval;
+        spawnEnemies.Add(temp);
+
+    }
 
 }
